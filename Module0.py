c68d8d44db4647ccb24fc14d356c4428a867fb66
EkaBond <ekabond87@gmail.com>
2024-10-13 16:19:51 +0300

    Add files via upload

---
+++ Module0.py
@@ -0,0 +1,34 @@
+# Task1
+print('1st program')
+print(9 ** 0.5 * 5)
+
+#Task2
+print("2nd program")
+print(9.99 >= 9.98 and 1000 != 1000.1)
+
+#Task3
+print("3rd program")
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+#Task4
+print("4th program")
+print(float('123.456'))  #step1
+print(float('123.456') * 10)  #step2
+print(int(float('123.456') * 10))    #step3
+print(int(float('123.456') * 10 % 10))  #step result
+
+#Taskbonus1
+print((1234 // 10) % 100)  #step1
+print((5678 // 10) % 100)  #step2
+print(((1234 // 10) % 100) + ((5678 // 10) % 100))  #step result
+
+#Taskbonus2
+print(int(13.42))    #step1
+print(int(42.13 * 100) % 100)    #step2
+print((int(13.42)) == (int(42.13 * 100) % 100))  #step3
+print(int(42.13))    #step4
+print(int(13.42 * 100) % 100)    #step5
+print((int(42.13)) == (int(13.42 * 100) % 100)) #step6
+print(((int(13.42)) == (int(42.13 * 100) % 100)) or ((int(42.13)) == (int(13.42 * 100) % 100)))  #step7
